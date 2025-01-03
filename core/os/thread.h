6e4ef0635409f36e0e4259ff3f403ee8244802da
HP van Braam <hp@tmm.cx>
2025-01-03 14:49:16 +0100

    Optimize Thread::get_caller_id()
    By making sure that Thread always has a valid caller_id we can remove the
    check making the function a straightforward getter instead.
    
    In some quick tests we see a repeatable performance improvement of
    somewhere around 0.32 mspf in TPS demo.
    
    Co-authored-by: Pedro J. Estébanez <pedrojrulez@gmail.com>
    (cherry picked from commit 873eb21ce81787fb67fb982684dc2bd5f71858ef)

--- core/os/thread.h
+++ core/os/thread.h
@@ -87,6 +87,7 @@ private:
 	friend class Main;
 
 	ID id = UNASSIGNED_ID;
+
 	static SafeNumeric<uint64_t> id_counter;
 	static thread_local ID caller_id;
 	THREADING_NAMESPACE::thread thread;
@@ -96,7 +97,7 @@ private:
 	static PlatformFunctions platform_functions;
 
 	static void make_main_thread() { caller_id = MAIN_ID; }
-	static void release_main_thread() { caller_id = UNASSIGNED_ID; }
+	static void release_main_thread() { caller_id = id_counter.increment(); }
 
 public:
 	static void _set_platform_functions(const PlatformFunctions &p_functions);
@@ -104,9 +105,6 @@ public:
 	_FORCE_INLINE_ ID get_id() const { return id; }
 	// get the ID of the caller thread
 	_FORCE_INLINE_ static ID get_caller_id() {
-		if (unlikely(caller_id == UNASSIGNED_ID)) {
-			caller_id = id_counter.increment();
-		}
 		return caller_id;
 	}
 	// get the ID of the main thread
